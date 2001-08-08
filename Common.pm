69af9f386bf8c31465fc1ecfa6ddb539c5547f0f
Terrence Brannon <metaperl@gmail.com>
2001-08-08 14:10:24 +0000

    Import of TBONE/Net-FTP-Common-2.0 from CPAN.
    gitpan-cpan-distribution: Net-FTP-Common
    gitpan-cpan-version:      2.0
    gitpan-cpan-path:         TBONE/Net-FTP-Common-2.0.tar.gz
    gitpan-cpan-author:       TBONE
    gitpan-cpan-maturity:     released

--- Common.pm
+++ Common.pm
@@ -11,173 +11,100 @@ use Net::FTP;
 
 use vars qw(@ISA $VERSION);
 
-@ISA = qw(Net::FTP);
+@ISA     = qw(Net::FTP);
 
-$VERSION = '1.9';
-
+$VERSION =   '2.0';
 
 # Preloaded methods go here.
 
 sub new {
     my $pkg  = shift;
-    my $netftp_cfg = shift;
-    my $common_cfg = shift;
+    my $common_cfg_in = shift;
+    my %netftp_cfg_in = @_;
 
-    my %common_cfg = (
-		      User => 'anonymous',
-		      Pass => 'list@rebol.com',
-		      Dir  => '/pub',
-		      Type => 'I'
-		     );
+    my %common_cfg_default = (
+			      User => 'anonymous',
+			      Host => 'ftp.microsoft.com',
+			      Pass => 'list@rebol.com',
+			      Dir  => '/pub',
+			      Type => 'I'
+			      );
 
-    my %netftp_cfg = ( Debug => 1, Timeout => 240 );
+    my %netftp_cfg_default = ( Debug => 1, Timeout => 240 );
 
-    @common_cfg{keys %$common_cfg} = values %$common_cfg;
-    @netftp_cfg{keys %$netftp_cfg} = values %$netftp_cfg;
+    # overwrite defaults with values supplied by sub input
+    @common_cfg_default{keys %$common_cfg_in} = values %$common_cfg_in;
+    @netftp_cfg_default{keys  %netftp_cfg_in} = values  %netftp_cfg_in;
     
     my $self = {};
 
-    @{$self->{Common}}{keys %common_cfg} = values %common_cfg;
-    @{$self->{NetFTP}}{keys %netftp_cfg} = values %netftp_cfg;
+    @{$self->{Common}}{keys %common_cfg_default} = values %common_cfg_default;
+    @{$self->{NetFTP}}{keys %netftp_cfg_default} = values %netftp_cfg_default;
 
-#    warn Data::Dumper->Dump([$self],['SELF']);
+    my $new_self = { %{$self->{NetFTP}}, Common => $self->{Common} } ;
 
-    bless $self, $pkg;
+    bless $new_self, $pkg;
 }
 
+sub Host { $_[0]->{Common}->{Host} or die "Host must be defined when creating a __PACKAGE__ object" }
 
+sub NetFTP { $_[0]->prep }
 
 sub login {
-  my ($self,$host) = @_;
+  my $self = shift;
 
-#  warn Data::Dumper->Dump([$self],['self(login)']);
-
-#  warn "HOST(login): $host";
-
-  my $ftp_session = 
-    Net::FTP->new($host, %{$self->{NetFTP}});
+  my $ftp_session = Net::FTP->new($self->Host, %$self);
 
   $ftp_session or return 0;
 
-  $ftp_session->login($self->{Common}->{User},$self->{Common}->{Pass}) 
-    && return $ftp_session 
-    || die "error logging in: $!";
-
+  $ftp_session->login($self->{Common}->{User}, $self->{Common}->{Pass})  && return $ftp_session  || die "error logging in: $!";
 }
 
 sub dir {
-    my ($self,$host) = @_;
+  my ($self, %config) = @_;
 
-    my $ftp = $self->login($host);
+  my $ftp = $self->prep($config{New});
 
-    use Data::Dumper; print Data::Dumper->Dump([$ftp],['ftp(dir)']);
-
-    $ftp->cwd($self->{Common}->{Dir});
-
-    my $ls = $ftp->ls;
-    if (!defined($ls)) {
-	return ();
-    } else {
-	return @{$ls};
-    }
+  my $ls = $ftp->ls;
+  if (!defined($ls)) {
+    return ();
+  } else {
+    return @{$ls};
+  }
 }
 
 sub mkdir {
-    my ($self,$host,%cfg) = @_;
+    my ($self,%config) = @_;
 
-    my $ftp = $self->login($host);
+    my $ftp = $self->prep($config{New});
 
-    print Data::Dumper->Dump(['cfg',\%cfg]);
-
-    $ftp->mkdir($cfg{Dir}, $cfg{Recurse});
+    $ftp->mkdir($config{Dir}, $config{Recurse});
 }
 
+# The Perl -e operator for files on a remote site. Even though the
+# REBOL exists? word works on local files as well as URL's, Perl's does 
+# not. Shame isn't it? :-)
 
-sub check {
-    my ($self,$host,%cfg) = @_;
+sub exists {
+    my ($self,%cfg) = @_;
 
-    my @listing = $self->dir($host);
+    my @listing = $self->dir(%cfg);
 
     scalar grep { $_ eq $cfg{File} } @listing;
 }
 
 sub glob {
 #    warn Data::Dumper->Dump([\@_],['@_(glob)']);
-    my ($self,$host,%cfg) = @_;
+    my ($self,%cfg) = @_;
 
 #    warn sprintf "self: %s host: %s cfg: %s", $self, $host, Data::Dumper::Dumper(\%cfg);
 
-    my @listing = $self->dir($host);
+    my @listing = $self->dir(%cfg);
 
     grep { $_ =~ /$cfg{File}/ } @listing;
 }
 
 sub grep { goto &glob }
-
-# The Perl -e operator for files on a remote site. Even though the
-# REBOL exists? word works on local files as well as URL's, Perl's does 
-# not. Shame isn't it? :-)
-
-sub file_exists {
-  my ($server, $user, $pass, $dir, $file) = @_;
-
-  my $ftp_session = ftp_login($server,$user,$pass);
-
-  $ftp_session->cwd($dir);
-
-  my @ls = ftp_session_ls($ftp_session);
-
-#  warn "* Grepping for $file in Listing @ls";
-
-  my @grep = (grep { $file eq $_ } @ls);
-  
-#  warn "* Grep @grep";
-
-  return (@grep > 0) ;
-}
-
-# Checking for a file on a remote FTP server by use of regular 
-# expressions as opposed to explicit filenames
-
-sub ftp_file_exists_re {
-  my ($server, $user, $pass, $dir, $file) = @_;
-
-  my $ftp_session = ftp_login($server,$user,$pass);
-
-  $ftp_session->cwd($dir);
-
-  my @ls = ftp_session_ls($ftp_session);
-
-#  warn "* re_Grepping for $file in Listing @ls";
-
-  my @grep = (grep { $_ =~ /$file/ } @ls);
-  
-#  warn "* Grep @grep";
-
-  return (@grep > 0) ;
-}
-
-# Login and upload a file to a directory.
-# In rebol this would be:
-# write/binary ftp://some.site/remote-file read/binary %local-file
-# But in Perl it's a many-line chore. Sigh.
-
-sub ftp_upload {
-
-  my ($server, $user, $pass, $dir, $file) = @_;
-
-  my $ftp_session = ftp_login($server,$user,$pass);
-
-  $ftp_session->cwd($dir);
-  if (!-e $file) {
-      die "$file does not exist on local disk. Cannot be uploaded";
-  } else {
-      warn "$file exists on local disk.. uploading";
-      $ftp_session->put($file);
-  }
-
-}
-
 
 # Really, the best abstraction would be:
 # ftp_download_all_with_hook where the hook in this case would be a
@@ -185,133 +112,212 @@ sub ftp_upload {
 # Laziness now will lead to more work later I guess.
 
 sub prep {
-  my ($self,$host) = @_;
+  my $self = shift;
+  my %cfg  = @_;
+
+  @{$self->{Common}}{keys %cfg} = values %cfg;
   
-  my $ftp = $self->login($host);
+  my $ftp = $self->login($self->Host);
   $ftp->cwd($self->{Common}->{Dir});
   $ftp->type($self->{Common}->{Type});
 
   $ftp;
 }
 
+sub binary {
+    my $self = shift;
+
+    $self->{Common}{Type} = 'I';
+}
+
+sub ascii {
+    my $self = shift;
+
+    $self->{Common}{Type} = 'A';
+}
 
 sub get {
 
-  my ($self,$host,%cfg) = @_;
+  my ($self,%cfg) = @_;
 
-  my $ftp = $self->prep($host);
+  my $ftp = $self->prep($cfg{New});
 
   $ftp->get($cfg{File},$cfg{LocalFile}) || die "download of $cfg{File} failed";
 
 }
 
 sub send {
+  my ($self,%cfg) = @_;
 
-  my ($self,$host,%cfg) = @_;
-
-  warn Data::Dumper->Dump([$self,$host,\%cfg],['self','host','cfg']);
-
-  my $ftp = $self->prep($host);
+  my $ftp = $self->prep($cfg{New});
 
   $ftp->put($cfg{File}) || die "upload of $cfg{File} failed";
-
 }
 
 
 1;
 __END__
-# Below is stub documentation for your module. You better edit it!
 
 =head1 NAME
 
-Net::FTP::Common - Perl extension for simplifying common usages of
-Net::FTP.
+Net::FTP::Common - Perl extension for simplifying common usages of Net::FTP.
 
 =head1 SYNOPSIS
 
   use Net::FTP::Common;
-  %net_ftp_config = ( Debug   => 1, Timeout => 120 );
+  %net_ftp_config = (Debug => 1, Timeout => 120);
 
-  %common_cfg     = 
+  %common_cfg = 
     (
-      User => 'username',          # overwrite anonymous user default
-      Pass => 'password',          # overwrite list@rebol.com pass default
-      Dir  => 'pub',               # overwrite slash default
-      Type => 'A'                  # overwrite I (binary) default
+      User => 'username',           # overwrite anonymous USER default
+      Host => 'ftp.perl.com',       # overwrite ftp.microsoft.com HOST default
+      Pass => 'tbone@soap-pan.org', # overwrite list@rebol.com PASS default
+      Dir  => 'pub',                # overwrite slash DIR default
+      Type => 'A'                   # overwrite I (binary) TYPE default
      );
 
-
-  $ez = Net::FTP::Common->new(\%net_ftp_config, \%common_cfg);
-
-  $host = 'ftp.rebol.com';
-
-  # Get a listing of a remote directory:
-  @listing =	$ez->dir($host);
-
-  # Make a directory on the remote machine
-  $ez->mkdir($host, Dir => '/pub/newdir/1/2/3', Recurse => 1);
-
-  # Get a file from the remote machine
-  $ez->get($host, File => 'codex.txt', LocalFile => '/tmp/crypto.txt');
-
-  # Send a file to the remote machine
-  $ez->send($host, File => 'codex.txt');
-
-  # test for a file's existence on the remote machine (using =~)
-  @file = $ez->grep($host, File => '[A-M]*[.]txt');
-  # a synonym for grep is glob (no difference, just another name)
-  @file = $ez->glob($host, File => 'n.*-file.t?t');
-
-  # test for a file on the remote machine (using eq)
-  $ez->check($host, File => 'needed-file.txt');
-  # note this is no more than you manually calling:
-  # (scalar grep { $_ = 'needed-file.txt' } $ez->dir($host)) > 0;
-  # or manually calling
-  # (scalar $ez->grep($host, File => 'needed-file.txt')) > 0
-
-
+  # NOTE WELL!!! one is passed by reference, the other by value.
+  # This is inconsistent, but still it is A Very, Very Good Thing.
+  # Believe me! I thought about this. And I have a good reason for it:
+  # This is to allow the least modification of legacy Net::FTP source code.
+  $ez = Net::FTP::Common->new(\%common_cfg, %net_ftp_config); 
 
   # can we login to the machine?
-  $ez->login($host) || die "cant login: $@";
+  # Note: it is NEVER necessary to first login before calling Net::FTP::Common API functions.                               
+  # This function is just for checking to see if a machine is up. It is published as part of
+  # The API because I have found it useful when writing FTP scripts which scan for the 
+  # first available FTP site to use for upload.
+  # The exact call-and-return semantics for this function are described and justified below.
+  $ez->login || die "cant login: $@";
+
+  # Get a listing of a remote directory (list the 'Dir' option supplied to the constructor)
+  @listing =	$ez->dir; 
+
+  # Let's list a different directory, over-riding and changing the
+  # default directory
+   @listing =	$ez->dir( New => { Dir => '/pub/rfcs' } ); 
+
+  # Let's list the default dir on several hosts
+  @host_listings = map { $ez->dir( New => { Host => $_ } ) } @host_list
+
+  # Let's get the listings of several directories
+  @dir_listings  = map { $ez->dir( New => { Dir  => $_ } ) } @dir_list;
+
+  # Get a file from the remote machine
+  $ez->get(File => 'codex.txt', LocalFile => '/tmp/crypto.txt');
+
+  # Send a file to the remote machine
+  $ez->send(File => 'codex.txt');
+
+  # test for a file's existence on the remote machine (using =~)
+  @file = $ez->grep(File => '[A-M]*[.]txt');
+
+  # a synonym for grep is glob (no difference, just another name)
+  @file = $ez->glob(File => 'n.*-file.t?t');
+
+  # test for a file on the remote machine (using eq)
+  $ez->exists(File => 'needed-file.txt');
+  # note this is no more than you manually calling:
+  # (scalar grep { $_ eq 'needed-file.txt' } $ez->dir) > 0;
 
 
 =head1 DESCRIPTION
 
 This module is intended to make the common uses of Net::FTP a one-line
 affair. Also, it made the development of Net::FTP::Shell 
-straightfoward.
+straightforward.
+
+Besides the constructor, all functions allow the over-riding of the initially supplied Net::FTP::Common configuration hashref 
+by supplying a hashref with an API call.
+
+Usage of this module is intended to be straightforward and stereotyped. The general steps to be used are:
+
+=over 4
+
+=item * use Net::FTP::Common
+
+=item * Define FTP configuration information 
+
+This can be inlined within the script but oftentimes this will be stored in a module for usage in many other scripts.
+
+=item * Use a Net::FTP::Common API function
+
+Note well that you NEVER have to login first. All API functions automatically log you in and change to the configured or
+specified directory. However, sometimes it is useful to see if you can actually login before
+attempting to do something else on an FTP site. This is the only time you will need the login() API method.
 
 Note well: though Net::FTP works in the stateful way that the FTP protocol 
 does, Net::FTP::Common works in a stateless "one-hit" fashion. That is, for
 each separate call to the API, a connection is established, the particular
 Net::FTP::Common functionality is performed and the connection is dropped.
 The disadvantage of this approach is the (usually irrelevant and 
-insignificant) over head of connection and disconnection. The
+insignificant) overhead of connection and disconnection. The
 advantage is that there is much less chance of incurring failure due
 to timeout.
 
-=head2 TRAPS FOR THE UNWARY
+=head1 METHODS
+
+=head2 new ( $net_ftp_common_hashref, %net_ftp_hash ) 
+
+This method takes initialization information for Net::FTP::Common as well as Net::FTP and returns a new Net::FTP::Common object.
+Though the calling convention may seem a bit inconsistent, it is actually the best API to support re-use of legacy
+Net::FTP constructor calls. For example if you had a Net::FTP script which looked like this:
+
+           use Net::FTP;
+
+           $ftp = Net::FTP->new("some.host.name", Debug => 0);
+           $ftp->login("anonymous",'me@here.there');
+           $ftp->cwd("/pub");
+           $ftp->get("that.file");
+           $ftp->quit;
+
+Here is all you would have to do to convert it to the Net::FTP::Common API:
+
+           use Net::FTP::Common;
+
+           $net_ftp_common_cfg = { Host => 'some.host.name', 
+				   User => 'anonymous',
+				   Pass => 'me@here.there',
+				   Dir  => '/pub'
+				   }	
+
+           $ftp = Net::FTP::Common->new($net_ftp_common_cfg, Debug => 0);
+           $ftp->get("that.file");
+           $ftp->quit;
+
+The example shows all the arguments that may be supplied to the
+Net::FTP::Common config hashref, except one, 'Type' which takes an
+argument of 'A' for ascii transfers and 'I' for binary transfers.
+
+=head2 dir 
+
+When given no arguments, C<dir()> uses Common configuration
+information to login to the ftp site, change directory and transfer
+type and then return an array of directory contents. You may only call
+this routine in array context and unlike Net::FTP, it returns a list
+representing the contents of the remote directory and in the case of
+no files, returns an empty array instead of (like Net::FTP) returning
+a 1-element array containing the element undef.
+
+You may give this function any number of configuration arguments to over-ride the predefined configuration options. For 
+example:
+
+ my %dir;
+ my @dir =qw (/tmp /pub /gnu);
+ map { @{$dir{$_}} = $ftp->dir(New => { Dir => $_ }) } @dir;
+
+=head1 TRAPS FOR THE UNWARY
 
 =item *
 
-  @file = $ez->grep($host, File => '[A-M]*[.]txt');
+  @file = $ez->grep(File => '[A-M]*[.]txt');
   
 is correct
 
-  @file = $ez->grep($host, '[A-M]*[.]txt');
+  @file = $ez->grep('[A-M]*[.]txt');
 
 looks correct but is not because you did not name the argument as you are 
 supposed to.
-
-=item * 
-
-In the current dain-bramaged interface, you can catch the bozak when you 
-accidentally forget to specify the $host argument to Net::FTP::Common's API 
-functions.   
-
-Since no one seems to be using this, I may in fact rework the API to allow the
-host to be a default instead of requiring it's specification.
-
 
 =head2 EXPORT
 
@@ -319,7 +325,7 @@ None by default.
 
 =head1 AUTHOR
 
-T. M. Brannon <tbone@cpan.org>
+T. M. Brannon <metaperl@yahoo.com>
 
 =head1 SEE ALSO
 
--- test.pl
+++ test.pl
@@ -10,6 +10,7 @@ BEGIN { $| = 1; print "1..1\n"; }
 END {print "not ok 1\n" unless $loaded;}
 use Net::FTP::Common;
 $loaded = 1;
+
 print "ok 1\n";
 
 ######################### End of black magic.
